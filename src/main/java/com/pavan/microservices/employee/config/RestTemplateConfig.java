b111d2bd12a08847308a9e740cd9dcb558595a4a
pavankumar <pavankumar.gspk9@gmail.com>
2026-06-07 11:46:19 +0530

    connection time out and read time out added to resttemplate

--- src/main/java/com/pavan/microservices/employee/config/RestTemplateConfig.java
+++ src/main/java/com/pavan/microservices/employee/config/RestTemplateConfig.java
@@ -2,13 +2,17 @@ package com.pavan.microservices.employee.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class RestTemplateConfig {
-	
+
 	@Bean
 	public RestTemplate getRestTemplate() {
-		return new RestTemplate();
+		SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+		factory.setConnectTimeout(3000);
+		factory.setReadTimeout(5000);
+		return new RestTemplate(factory);
 	}
 }
